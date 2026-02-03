3e413279b8c826848f6840c3d69738fd595706a5
Embitd0 <belandresjhonmark@gmail.com>
2026-02-03 09:31:43 +0800

    Revert "2.0.1"
    This reverts commit 3d95f399baa9f4feaba3cd10e3c633b20fb8a2e0.

--- scripts/event_handler.gd
+++ scripts/event_handler.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+# This line tells Godot to stop checking for unused signals in this file
+@warning_ignore("unused_signal")
+
 signal battle_started(enemy_node, character_name, lvl)
 signal battle_finished()
