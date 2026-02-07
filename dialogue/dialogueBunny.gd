f4bffb9db860c49b6437f47090d50addddcb8d35
Embitd0 <belandresjhonmark@gmail.com>
2026-02-08 01:03:30 +0800

    2.2.1
    added init cutscene and typewriter sfx

--- dialogue/dialogueBunny.gd
+++ dialogue/dialogueBunny.gd
@@ -7,6 +7,8 @@ var dialogue = []
 var currentDialogueID = 0 #identifier for current dialogue
 var dActive = false
 
+var type_speed = 0.05
+
 func _ready():
 	$NinePatchRect.visible = false
 	
@@ -28,7 +30,11 @@ func _input(event):
 	if !dActive:
 		return
 	if event.is_action_pressed("ui_accept"):
-		next_script()
+		if $AnimationPlayer.is_playing():
+			$AnimationPlayer.stop()
+			$NinePatchRect/Text.visible_ratio = 1.0
+		else:
+			next_script()
 		
 func next_script():
 	currentDialogueID += 1
@@ -39,5 +45,18 @@ func next_script():
 		return
 		
 	$NinePatchRect/Name.text = dialogue[currentDialogueID]['Name']
-	$NinePatchRect/Text.text = dialogue[currentDialogueID]['Text']
+	var current_text = dialogue[currentDialogueID]['Text']
+	$NinePatchRect/Text.text = current_text
+	
+	$NinePatchRect/Text.visible_ratio = 0.0
+	
+	var duration = current_text.length() * type_speed
+	
+	if duration > 0:
+		$NinePatchRect/AnimationPlayer.speed_scale = 1.0 / duration
+	else:
+		$AnimationPlayer.speed_scale = 1.0
+
+	$NinePatchRect/AnimationPlayer.play("typewriterfx")
+		
 	
